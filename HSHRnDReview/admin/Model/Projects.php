de68d2f7d5ff48092b549517a25ca8fbe832e1fd
Jens Kowal <jenskowal@mac.com>
2016-03-08 11:52:54 +0100

    some bugfixes

--- HSHRnDReview/admin/Model/Projects.php
+++ HSHRnDReview/admin/Model/Projects.php
@@ -5,7 +5,7 @@
  * @license		GNU General Public License version 2 or later
  */
 
-namespace HSH\HSHRnDReview\Site\Model;
+namespace HSH\HSHRnDReview\Admin\Model;
 
 use FOF30\Container\Container;
 use FOF30\Model\DataModel;
@@ -13,37 +13,20 @@ use FOF30\Model\DataModel;
 class Projects extends DataModel
 {
 	public function buildQuery($overrideLimits = false)
-	{
-		$query = parent::buildQuery($overrideLimits);
+    {
+	    $query = parent::buildQuery($overrideLimits);
 
-		// Manipulate $query to select more fields, add your joins and everything else you please. $query is a standard JDatabaseQuery object.
-		
-		//select * from pk3mz_hshrndreview_projects, pk3mz_hshrndreview_statuses where pk3mz_hshrndreview_projects.hshrndreview_project_id = pk3mz_hshrndreview_statuses.hshrndreview_project_id AND pk3mz_hshrndreview_statuses.isMostCurrent = 1
-		
-		
-//		$query
-//		    ->select(array('a.*', 'b.username', 'b.name'))
-//		    ->from($db->quoteName('#__content', 'a'))
-//		    ->join('INNER', $db->quoteName('#__users', 'b') . ' ON (' . $db->quoteName('a.created_by') . ' = ' . $db->quoteName('b.id') . ')')
-//		    ->where($db->quoteName('b.username') . ' LIKE \'a%\'')
-//		    ->order($db->quoteName('a.created') . ' DESC');
-		
-		
-		
-		$query
-		    ->select(array('p.*', 's.*'))
-		    ->from($db->quoteName('#__hshrndreview_projects', 'p'))
-		    ->join('INNER', $db->quoteName('#__hshrndreview_statuses', 's') . ' ON (' . $db->quoteName('p.hshrndreview_project_id') . ' = ' . $db->quoteName('s.hshrndreview_project_id') . ')');
+	    // Manipulate $query to select more fields, add your joins and everything else you please. $query is a standard JDatabaseQuery object.
 
-//		    ->where($db->quoteName('b.username') . ' LIKE \'a%\'')
-//		    ->order($db->quoteName('a.created') . ' DESC');
-		
-		
-		
-		
-		
+		//SQL Query:
+        //select * from pk3mz_hshrndreview_projects, pk3mz_hshrndreview_statuses where pk3mz_hshrndreview_projects.hshrndreview_project_id = pk3mz_hshrndreview_statuses.hshrndreview_project_id
 
-		return $query;
+		//$db=$this->getDbo();
+		//              $query
+		//                  ->select(array('p.*', 's.*'))
+		//                  ->from($db->quoteName('#__hshrndreview_projects', 'p'))
+		//                  ->join('INNER', $db->quoteName('#__hshrndreview_statuses', 's') . ' ON (' . $db->quoteName('p.hshrndreview_project_id') . ' = ' . $db->quoteName('s.hshrndreview_project_id') . ')');
+	    return $query;
 	}
 
 }
